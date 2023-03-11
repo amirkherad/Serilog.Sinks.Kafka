00d45f6586e45a376337316e6b4cb2fca55db767
Amir Kheradmand Rad <a.kheradmandrad@asanpardakht.ir>
2023-03-11 11:27:48 +0330

    Remove weatherforecast files

---
+++ SampleWebApi/Controllers/TestController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace SampleWebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestController : ControllerBase
+    {
+        private readonly ILogger<TestController> _logger;
+
+        public TestController(ILogger<TestController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            _logger.LogInformation("Info log test!");
+            _logger.LogError("Error log test!");
+            _logger.LogWarning("Warning log test!");
+
+            return Ok();
+        }
+    }
+}
